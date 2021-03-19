c2aa4181422603bb4c30d08a0ae3e70e6ac2aad5
LahsiniMohamed <lahsini9915@gmail.com>
2021-03-19 11:25:39 +0100

    added new class and modified the App class

---
+++ src/Client.java
@@ -0,0 +1,9 @@
+public class Client {
+    private String name ;
+    public Client(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+}
