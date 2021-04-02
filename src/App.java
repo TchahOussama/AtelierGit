38812dcda495743f666678266afc22eb63a00fe2
LahsiniMohamed <36932931+LahsiniMohamed@users.noreply.github.com>
2021-04-02 11:38:30 +0100

    updated the App class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Oussama!");
+        System.out.println("Hello, Oussama and Mohamed!");
     }
 }
